79805f231f48684a4b66cadeeac2678b43eafca2
Søren Lund <soren.lund@amesto.dk>
2022-12-29 16:50:09 +0100

    Test will throw error, since the service is called 3 times

--- MoqMyWordsTesting/MoqMyWordTests.cs
+++ MoqMyWordsTesting/MoqMyWordTests.cs
@@ -1,9 +1,17 @@
 using Moq;
+using Xunit.Abstractions;
 
 namespace MoqMyWordsTesting
 {
     public class MoqMyWordTests
     {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public MoqMyWordTests(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
         [Fact]
         public async Task MoqMyWord_UpdateService_OnlyCalledOnce()
         {
@@ -21,6 +29,8 @@ namespace MoqMyWordsTesting
 
             await priceService.UpdateOnlyNewestPrice(productList);
 
+            //For the sake of the argument a silly way to evaluate any price less than 4 is valid
+            //This is only to make sure the test fails.
             service.Verify(u => u.UpdateAsync(It.Is<Product>(p => EvaluatePrice(p))),Times.Once);
 
             await Task.CompletedTask;
@@ -28,7 +38,8 @@ namespace MoqMyWordsTesting
 
         private bool EvaluatePrice(Product product)
         {
-            return product.Price == 1;
+            _outputHelper.WriteLine($"product price is {product.Price}");
+            return product.Price < 4;
         }
     }
 }
