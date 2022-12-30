0bf85f8d81ba0a57ef07b6a2a532be1cb821ace5
Søren Lund <soren.lund@amesto.dk>
2022-12-30 09:49:19 +0100

    Changing the implementation

--- MoqMyWords/Program.cs
+++ MoqMyWords/Program.cs
@@ -13,9 +13,11 @@ public class PriceService
 
     public async Task<Product> UpdateOnlyNewestPrice(List<Product> products)
     {
+        var product = new Product { Id = 1, Name = "Cool Product" };
         foreach (var p in products.OrderByDescending(o => o.Created).Take(3))
         {
-            await _inventoryService.UpdateAsync(p);
+            product.Price = p.Price;
+            await _inventoryService.UpdateAsync(product);
         }
 
         return products.FirstOrDefault() ?? new Product();
